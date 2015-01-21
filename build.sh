e656ac1b809b6818b115c22ba634c255e443a587
jaumeortola <jaumeortola@gmail.com>
2015-01-21 15:49:50 +0100

    Resol problema amb mots al·l-; millora de l'script

--- build.sh
+++ build.sh
@@ -5,5 +5,14 @@ echo "******** RESULTS ********"
 cat resum-resultats.txt
 echo "*************************"
 echo "Converting patterns..."
-perl substrings.pl hyph-ca-new2.tex output.dic ISO-8859-1 1 1 > substrings-results.txt
-echo "Done."
+perl substrings.pl hyph-ca-new2.tex output.dic > substrings-results.txt
+#Converteix a format DOS / UTF-8
+iconv -f ISO-8859-1 -t UTF-8 output.dic > output-utf8.dic
+sed -i 's/$/\r/' output-utf8.dic
+#Afegeix la capçalera
+cat hyph_ca_ES-header.dic output-utf8.dic > hyph_ca_ES.dic
+#Elimina fitxers intermedis
+rm hyph-ca-new2.tex
+rm output.dic
+rm output-utf8.dic
+echo "Done. Output: hyph_ca_ES.dic"
